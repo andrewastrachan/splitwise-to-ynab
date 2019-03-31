28fe6780a093a705f3f87bbc9101fdac1857026e
Andrew Strachan <andrewastrachan@outlook.com>
2019-03-31 12:28:01 -0700

    initial commit

---
+++ babel.config.js
@@ -0,0 +1,32 @@
+module.exports = function (api) {
+  const babelEnv = api.env();
+
+  const presets = [
+    [
+      '@babel/env',
+      {
+        targets: '> 1% in US, last 2 versions, not dead',
+        useBuiltIns: 'usage'
+      }
+    ]
+  ];
+
+  const plugins = [
+    '@babel/plugin-proposal-object-rest-spread',
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        root: ['./src/'],
+        alias: {
+          '*shared': './src/shared',
+          '*errors': './src/errors'
+        }
+      }
+    ]
+  ];
+
+  return {
+    presets,
+    plugins
+  };
+};
---
+++ src/main.js
@@ -0,0 +1,3 @@
+module.exports = function convertFromSplitwiseToYnab(filePath) {
+
+}
